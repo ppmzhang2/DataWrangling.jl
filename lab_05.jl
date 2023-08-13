b230891b244b43c8cb500ebddccfd13823349d01
ppmzhang2 <ppmzhang2@users.noreply.github.com>
2023-08-13 10:26:56 +0000

    deploy: 48c677bb7f859f169beca4e530f94cd73f36e78c

---
+++ lab_05.jl
@@ -0,0 +1,26 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1c405ed8-30ba-11ee-34e3-8d9c2987354e
+md"""
+# Lab 5. Stringing Julia together with Dates and all the rest
+
+Most of the operations that R can do on strings, and more, are available for Julia.
+
+As the R lab is long, here I only suggest you some readings and activities in Julia.
+
+**Strings** operations are in the base Julia system.
+You can read more [here](https://docs.julialang.org/en/v1/manual/strings/index.html).
+
+**Mapping** and **iterations** also are in the base system.
+You can read more [here](https://docs.julialang.org/en/v1/base/collections/).
+
+Moreover, as Julia gets compiled, loops (for, while, ...) are extremely performing.
+Where in `R` it is not suggested to write in Loops, in Julia is perfectly fine :-)
+"""
+
+# ╔═╡ Cell order:
+# ╟─1c405ed8-30ba-11ee-34e3-8d9c2987354e
